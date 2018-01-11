962798544218e601137b46f337d2f3290eaf3009
szhakupbekov <szhakupbekov@woppay.com>
2018-01-11 21:36:58 +0600

    Обновление для gh-pages

---
+++ desktop/app/js/activateRoomName.js
@@ -0,0 +1,40 @@
+import {getCoords, getNodeFromMarkup} from './helpers';
+
+export default () => {
+  const diagramBody = document.querySelector('.diagram__body');
+  const diagramBodyCnt = document.querySelector('.diagram__body-cnt');
+  const body = document.querySelector('body');
+
+  if (diagramBody === null) {
+    return;
+  }
+  const diagramRoomArr = diagramBodyCnt.querySelectorAll('.diagram__room');
+
+  for (let diagramRoom of diagramRoomArr) {
+    const diagramRoomName = diagramRoom.querySelector('.diagram__room-name');
+    const roomNameTag = `<div class="room-name-tag">${diagramRoomName.innerHTML}</div>`;
+    const roomNameTagNode = getNodeFromMarkup(roomNameTag);
+    if (diagramRoom.classList.contains('diagram__room--filled')) {
+      roomNameTagNode.classList.add('filled');
+    }
+    diagramRoom.appendChild(roomNameTagNode);
+  }
+
+  diagramBody.addEventListener('scroll', () => {
+    const elCoordsLeft = -1 * getCoords(diagramBodyCnt).left;
+    const roomNameTagArr = document.querySelectorAll('.room-name-tag');
+    const diagramRowBody = document.querySelector('.diagram__row-body');
+    const diagramRowBodyLeftCoords = getCoords(diagramRowBody).left;
+
+    if (elCoordsLeft > 180) {
+      for (let roomNameTag of roomNameTagArr) {
+        roomNameTag.classList.add('taged');
+        roomNameTag.style.transform = `translateX(${-1 * diagramRowBodyLeftCoords + 180}px)`;
+      }
+    } else {
+      for (let roomNameTag of roomNameTagArr) {
+        roomNameTag.classList.remove('taged');
+      }
+    }
+  });
+};
